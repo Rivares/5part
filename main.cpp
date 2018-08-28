44ff817a68aa958d1b9b536b16a209e1439dc889
Rivares <ura-nikolaevic@yandex.ru>
2018-08-29 02:11:10 +0300

    Fixed bugs. Restored functional.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     MainWindow w;
-    w.setFixedSize(QSize(890, 600));
+    w.setFixedSize(QSize(840, 550));
     w.show();
 
     return a.exec();
